ff55d49a8ac086528dcdb2461e18eb702022570c
Eric Lin <40250570+Ericlin415@users.noreply.github.com>
2020-06-04 19:09:19 -0700

    Add files via upload

---
+++ javascript-practice-1.js
@@ -0,0 +1,38 @@
+// practice 1
+
+
+
+/* operators that take more than 2 values are called * binary operators *.
+operators that take in 1 value is called a * unary operators *.
+
+*/
+
+//console.log(true == true);
+
+// although both types are the same, they are not equal.
+// console.log(NaN == NaN); 
+
+// There are three logical operators: Unary, binary, ternary.
+// example of a ternary operator:
+
+
+// when the boolen type is true, then the middle value is returned.
+
+// console.log( true ? 1: 2 );
+// console.log( true ? 1 : 2 );
+
+// when the boolean type is false, then the right most value is returned.
+
+// console.log( false ? 1 : 2 );
+// console.log( false ? 1 : 2 );
+
+// Automatic Type Conversion
+
+console.log( "5" - 1);
+
+
+
+
+
+
+
