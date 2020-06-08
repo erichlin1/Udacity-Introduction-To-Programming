7ce290750ae4c8c2307fa1aff2c905d5922c48cc
Alyssa Lee <auliee@BoVangs-iMac.local>
2020-06-08 09:41:22 -0700

    shell workshop

---
+++ python/shell-workshop.py
@@ -0,0 +1,6 @@
+# Graphical User Interface / GUI
+# Command-Line Interface / CLI
+# Shell - The outermost layer of your operating system
+# A user interface where you give instructions to the computer by entering lines of text.
+# echo - simliar to printf for javascript
+# Use (!!) to repeat command on the CLI
