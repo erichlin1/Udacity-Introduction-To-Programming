21b5bf25928c1993dea41753b942a0120513318f
Alyssa Lee <auliee@BoVangs-iMac.local>
2020-06-09 16:55:09 -0700

    Javascript practice

---
+++ Javascript/javascript-unmerged.js
@@ -0,0 +1,84 @@
+
+// ** Variable BINDING **
+
+/* Binding a value uses the keyword (var, let, const) where var was used pre-2015 JS
+ and let is scope defined. const implies that a value is bound indefinitely.
+
+ constant binding => const declaration
+
+ - The environment implies there exist a set of bindings and their values. 
+ - executing a function is called invoking.
+ - command + option + I (inspection or Javascript console);
+ - side effects vs return value
+
+
+*/
+
+
+// ** Null, Undefined, NaN **
+
+/* NaN = Not a number, 'Undefined' is returned when a variable is not declared.
+Null is simliar to 'Undefined' Treat both Null and 'Undefined' the same. 
+*/
+
+
+// Escaping Character - ES6 string literal - backtick basics
+// template literal - string literals that allow for embedded expressions (template substitute)
+
+// ** Implicit type coercion **
+
+/* As the name states, A integer declared as a string data type will be converted into 
+a integer data type. Coercion => forced conversion. 
+
+- Javascript is a loosely typed language. Does not require data types to be classified.
+- C is not a loosely typed language. 
+
+*/
+
+/* Strongly typed language vs Loosely typed language.
+STL generates more errors due to its restriction of data type conversion. 
+the data type in question has to represents the expects data type.
+
+Where as...
+
+LTL generates less error, but leads to more error. 
+*/
+
+// ---- Exercise ---- //
+
+const age = 28;
+let escapingCharacter = `Eric is ${age}.` // variable called Template Literal
+console.log(escapingCharacter)
+// every character in a string literal has correpsonding ASCII value.
+let myString = `blue`;
+let index = 0;
+let characterCount = 0;
+for (index; index < myString.length; index++)
+{
+    console.log(myString.charCodeAt(index));
+};
+
+// ---- Exercise ---- //
+
+// instantiating an object requires property, and value declartion;
+
+const object1 = {
+    name:'Eric',
+    age: 28,
+    ethnicity: 'Chinese'
+};
+
+console.log(Object.getOwnPropertyNames(object1));
+
+// ---- Exercise ---- //
+
+console.log(String('Hello'));
+console.log(Number("Hello")); // => "Hello" cannot be converted into a legal Number. compare this to the following..
+console.log(Number("10")); // => 10.
+
+
+
+
+
+
+
