9367e18ef78098e83d278bb5a954bce30a4d094d
Alyssa Lee <auliee@BoVangs-iMac.local>
2020-06-14 20:07:47 -0700

    test

---
+++ Javascript/lesson-2.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: First Expression (2-1)
+ * Write an expression that uses at least three, distinct, arithmetic operators
+ * to log the number 42 to the console..
+ */
+
+console.log((20 / 2) + 12 + (10 * 2));
+  
+ 
+/*
+ * QUIZ REQUIREMENTS..
+ * 1. Your code should print the value `42`..
+ * 2. You should use at least 3 distinct operators. (`+`, `-`, `*`, `/`, or `%`)..
+ * 3. Your code should not be empty..
+ */
+
+
+// this expression equals 4, change it to equal 42..
+console.log((1 + 5 - 2) + 38);
