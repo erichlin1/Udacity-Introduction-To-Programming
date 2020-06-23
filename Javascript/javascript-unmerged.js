9cd389b50c899f344803d0975166c5e4b2244466
Alyssa Lee <auliee@BoVangs-iMac.local>
2020-06-22 17:53:56 -0700

    EJS Page: 57; L7.3

--- Javascript/javascript-unmerged.js
+++ Javascript/javascript-unmerged.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 const { type } = require('os');
 const { TestResult } = require('@jest/types');
+const { is } = require('@babel/types');
 // const { NONAME } = require('dns');
 // const { CLIENT_RENEG_LIMIT } = require('tls');
 
@@ -1290,7 +1291,6 @@ test.forEach((score, index, array) => {
     if (score % 3 === 0) {
         array[index] = score + 100;
     };
-    console.log(score);
 });
 
 /* forEach Method using fuction declaration and explicity invoking callback function using forEach Method on array. */
@@ -1301,15 +1301,76 @@ function testScores(score, index, array) {
     console.log(score);
 }
 
-test.forEach(testScores);
 
 /* 
 *.map method execution 
 * compared to forEach method, map method returns an array.
 */
+var numbers = [1, 2, 3, 5, 6];
+function multipleByTwo(number, index, array) {
+    // each line is executed to the extend of the length of the array.
+    number = number * 2; 
+    console.log(number);
+    return number;
+}
+// inline function expressions using ES6 for map function
+
+const isMultipliedByTw0 = numbers.map((number) => {
+    number *= 2;
+    return number;
+});
 
 
+/* QUiz: I Got Bills
+*
+*/
 
+var bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90, 29.11, 12.99, 10.00, 99.22, 102.20, 100.10, 6.77, 2.22];
+
+/* 
+* declared inline function expression stored in identifier totals 
+*
+*
+*/
+const totals = bills.map((bill) => {
+    return Number((bill + (bill * 0.15)).toFixed(2));
+});
+console.log(totals);
+
+
+/*  nested numbers
+*      
+*
+*/
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
+
+(function nestedNumbers(numbers) {
+    let newNumbers = [];
+    const numbersLen = numbers.length;
+    for (let i = 0; i < numbersLen; i++) {
+        let tempList = [];
+        for (j = 0; j < numbers[i].length; j++) {
+            numbers[i][j] % 2 === 0 ? tempList.push('even') : tempList.push('odd');
+        };
+        newNumbers.push(tempList);
+    };
+})();
+
+/* 
+* method is just a function that is associated with an object. 
+*
+*/
 
 
 
